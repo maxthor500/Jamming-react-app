f329030db376051e2b89b9fdd64b8efd02944f7a
maxthor500 <iraubuntu@gmail.com>
2021-06-04 13:12:01 +0100

    Add SearchResults component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 
 class App extends React.Component {
   render() {
@@ -7,9 +9,9 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* <SearchBar /> */}
+          <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults /> */}
+            <SearchResults />
             {/* <Playlist /> */}
           </div>
         </div>
