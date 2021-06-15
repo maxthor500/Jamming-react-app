1261c22bd25c5485d1cea8db317855d4c59ed62e
maxthor500 <iraubuntu@gmail.com>
2021-06-15 22:23:50 +0100

    Obtain a Spotify Access Token

---
+++ src/util/Spotify.js
@@ -0,0 +1,32 @@
+import clientID from "../../clientID";
+
+const clientID = clientID;
+const redirectUri = 'http://localhost:3000/'
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch;
+            const expiresIn = Number(expiresInMatch[1]);
+            // This clear the parameter, allow us to grab a new access token whet it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    }
+}
+
+
+export default Spotify;
